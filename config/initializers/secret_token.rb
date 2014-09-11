e0329963e28e448cdeac26d5d97b1ac66719a8be
youstina <youstina.atef@bibalex.org>
2014-09-11 14:31:27 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EolTranslationToolRails::Application.config.secret_token = '4c6efcb5e8cfb397b0f4439f96552005cebf7224c352f75324c915f05d35e1886f6be56af3aabb78192322b7ca874cd2e0a9039624f0daddd32b731961e1466a'
